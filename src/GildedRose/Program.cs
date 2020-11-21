7f50bc7fe36a946a34f46f3fe86a7a5a6d09382f
Bogdan Polak <bogdanpol@gmail.com>
2020-11-21 22:54:08 +0000

    Extraced Logic/Item class

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using GildedRose.Logic;
 
 namespace GildedRose
 {
-    public class Item
-    {
-        public string Name { get; set; }
-        public int SellIn { get; set; }
-        public int Quality { get; set; }
-        public override string ToString() =>
-            $"{Name}   [sell in days: {SellIn}] [quality: {Quality}";
-    }
 
     public class Program
     {
--- src/GlidedRoseTest/TestsQualityUpdater.cs
+++ src/GlidedRoseTest/TestsQualityUpdater.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using GildedRose;
 using System.Collections.Generic;
 using System.Linq;
+using GildedRose.Logic;
 
 namespace GlidedRoseTest
 {
